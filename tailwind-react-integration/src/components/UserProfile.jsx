b10a66fdf0bb9056ed9e095070a20f3f863797ae
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-09-28 18:57:08 +0300

    New

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,16 +8,18 @@ function UserProfile() {
         text-center
         shadow-lg rounded-lg
         max-w-xs md:max-w-sm
-        p-4 md:p-8 sm:p-4
+        p-4 sm:p-4 md:p-8
         hover:shadow-2xl hover:-translate-y-2 transition transform duration-300
       "
     >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full mx-auto w-24 h-24 sm:w-36 sm:h-36"
+        className="rounded-full mx-auto w-24 h-24 md:w-36 md:h-36"
       />
-      <h3 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h3>
+      <h3 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">
+        John Doe
+      </h3>
       <p className="text-sm sm:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new
         technologies.
