7ce6efdefd07f1c46ed52261202b979fa66b3d5e
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-09-28 18:52:54 +0300

    New

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,12 +2,12 @@ function UserProfile() {
   return (
     <div
       className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center 
-                hover:shadow-2xl hover:-translate-y-2 transition transform duration-300"
+                    hover:shadow-2xl hover:-translate-y-2 transition transform duration-300"
     >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-[150px] h-[150px] mx-auto"
+        className="rounded-full w-36 h-36 mx-auto"
       />
       <h3 className="text-xl text-blue-800 my-4">John Doe</h3>
       <p className="text-base text-gray-600">
