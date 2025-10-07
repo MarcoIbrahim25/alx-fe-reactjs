18dbad02b9a69983a433ed3435a154d6e9b829bf
MarcoSerry <marcoserry88@gmail.com>
2025-10-07 17:00:39 +0300

    New

---
+++ react-todo/src/tests/TodoList.test.js
@@ -0,0 +1,38 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+function setup() {
+  render(<TodoList />);
+}
+
+test("renders initial demo todos", () => {
+  setup();
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Write tests")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  setup();
+  const input = screen.getByLabelText("todo-input");
+  const form = screen.getByLabelText("add-form");
+  fireEvent.change(input, { target: { value: "New task" } });
+  fireEvent.submit(form);
+  expect(screen.getByText("New task")).toBeInTheDocument();
+});
+
+test("toggles a todo completed state", () => {
+  setup();
+  const span = screen.getByText("Learn React");
+  const checkbox = screen.getAllByRole("checkbox")[0];
+  expect(span).toHaveStyle({ textDecoration: "none" });
+  fireEvent.click(checkbox);
+  expect(span).toHaveStyle({ textDecoration: "line-through" });
+});
+
+test("deletes a todo", () => {
+  setup();
+  const btn = screen.getByLabelText(/delete-/i);
+  fireEvent.click(btn);
+  expect(screen.queryByText("Write tests")).not.toBeInTheDocument();
+});
